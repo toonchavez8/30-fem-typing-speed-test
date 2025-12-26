4a8cc421722e932cfd8dfd127fe47ea25bbeae29
toonchavez8 <desarrollador5@sistemasicong.com>
2025-12-26 15:59:31 -0600

    feat(app): add base layout and move icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen items-center justify-center bg-zinc-50  dark:bg-black">
+		<main className="flex items-center justify-center bg-zinc-50  dark:bg-black">
 			test font
 		</main>
 	);
---
+++ components/footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+	return (
+		<footer className="debug absolute -bottom-20 group-hover:bottom-0 transition-all ease-in  w-full text-center py-4 text-sm">
+			Challenge by{" "}
+			<a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
+			Coded by <a href="./">Your Name Here</a>.
+		</footer>
+	);
+};
+export default Footer;
