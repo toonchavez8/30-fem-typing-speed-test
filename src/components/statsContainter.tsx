b752281b1485095649b36fbf23d8ae27909542c4
Miguel Chavez <toonchavez8@gmail.com>
2025-12-28 21:15:16 -0600

    refactor(app): add gsap animation to passage display

--- src/components/statsContainter.tsx
+++ src/components/statsContainter.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { useRef } from "react";
-import { useGame } from "./GameContext";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useGame } from "./GameContext";
 
 type Mode = "timed" | "passage";
 type Difficulty = "easy" | "medium" | "hard";
